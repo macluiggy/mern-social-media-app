26354c81063bb287018adb78dd6f0b8120995463
luiggy macias <ferrinluiggy@gmail.com>
2022-02-14 21:50:22 -0500

    ddd

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,4 +1,4 @@
-const isProduction = !true;
+const isProduction = true;
 export const path = isProduction
-  ? "https://backend-social-media-app.herokuapp.com"
+  ? "https://macluiggy-social-media-app.herokuapp.com"
   : "http://localhost:3000";
